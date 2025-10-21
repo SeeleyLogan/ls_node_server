90c42b37ac70fd812e35376454724c390a288c93
Logan Seeley <seeley.logan@outlook.com>
2025-10-20 22:23:28 -0400

    License

--- src/shared.c
+++ src/shared.c
@@ -13,3 +13,21 @@ INLINE socket_t socket_init6(void)
 
     return new_socket;
 }
+
+
+/*
+ * Copyright (C) 2025  Logan Seeley
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
